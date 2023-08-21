2b997a07b73fa39a12ceff02b941580dc56dd6be
Ian Richards <cdeveloper@hotmail.co.uk>
2023-08-21 22:53:06 +0100

    Added in memory database and fixed issue with interface to concrete mappings

--- TP24.API/Program.cs
+++ TP24.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TP24.Data.Contexts;
+using TP24.Data.Interfaces;
 using TP24.Data.Repositories;
 using TP24.Services.Interfaces;
 using TP24.Services.Mappers;
@@ -5,14 +8,17 @@ using TP24.Services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IRepository, PayloadRepository>();
-builder.Services.AddScoped<IPayloadService, PayloadService>();
 builder.Services.AddScoped<IPayloadMapper, PayloadMapper>();
+builder.Services.AddScoped<IStatisticsService, StatisticsService>();
+builder.Services.AddScoped<IPayloadService, PayloadService>();
+builder.Services.AddScoped<IPayloadRepository, PayloadRepository>();
+builder.Services.AddScoped<IStatisticsRepository, StatisticsRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<TpContext>(options => options.UseInMemoryDatabase(databaseName: "AuthorDb"));
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
